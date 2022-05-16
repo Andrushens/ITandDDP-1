214451175809bbb4e783517da07579d60ce29f65
andrushens <thelasmo3228@gmail.com>
2022-05-16 23:23:18 +0300

    update css styles & html

--- js/home.js
+++ js/home.js
@@ -1,14 +1,14 @@
-var isOpen = false;
+var isDialogsShown = false;
 
 document.getElementById("display-dialogs-button").onclick = function () {
     var button = document.getElementById("display-dialogs-button");
     var list = document.getElementById("chat-list");
-    if (isOpen) {
+    if (isDialogsShown) {
         list.style.display = "none";
         button.innerHTML = '>';
     } else {
         list.style.display = "flex";
         button.innerHTML = '<';
     }
-    isOpen = !isOpen;
+    isDialogsShown = !isDialogsShown;
 };
