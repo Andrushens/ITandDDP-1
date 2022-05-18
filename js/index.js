a3e1e0dc912c620aa08d092de4a8bcad0a5d4bf1
andrushens <thelasmo3228@gmail.com>
2022-05-18 14:23:35 +0300

    update redirect to home path

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 document.getElementById("login-button").onclick = function () {
     let login = document.getElementById("login-textarea");
-    window.location.href = `http://127.0.0.1:5500/home.html?login=${login.value}`;
+    window.location.href = `https://andrushens.github.io/ITandDDP-1/home.html?login=${login}`;
 };
