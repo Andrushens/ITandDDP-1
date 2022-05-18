4276440a3f6a5ccc2305e59b93909e3ab4fdb794
andrushens <thelasmo3228@gmail.com>
2022-05-18 14:30:01 +0300

    fix redirect path

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 document.getElementById("login-button").onclick = function () {
     let login = document.getElementById("login-textarea");
-    window.location.href = `https://andrushens.github.io/ITandDDP-1/home.html?login=${login}`;
+    window.location.href = `https://andrushens.github.io/ITandDDP-1/home.html?login=${login.value}`;
 };
