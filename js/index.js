64d0ac7b920cb9b2917405513e593185095bf4ee
andrushens <thelasmo3228@gmail.com>
2022-05-18 14:21:11 +0300

    add js functionality

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
 document.getElementById("login-button").onclick = function () {
-    window.location.href = "https://andrushens.github.io/ITandDDP-1/home.html";
+    let login = document.getElementById("login-textarea");
+    window.location.href = `http://127.0.0.1:5500/home.html?login=${login.value}`;
 };
